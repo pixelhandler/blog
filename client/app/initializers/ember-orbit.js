e4feb5662c4e319402ed2c20886226153ee27d8b
Bill Heaton <bill@mediaartslab.com>
2014-08-04 11:38:06 -0700

    Connect Memory and JSONAPI sources

--- client/app/initializers/ember-orbit.js
+++ client/app/initializers/ember-orbit.js
@@ -22,8 +22,33 @@ export default {
 
   initialize: function(container, application) {
     application.register('schema:main', Schema);
-    application.register('store:main', JSONAPIStore);
+    application.register('store:main', EO.Store);
+    application.register('store:jsonApi', JSONAPIStore);
+    connectSources(container);
+
     application.inject('controller', 'store', 'store:main');
     application.inject('route', 'store', 'store:main');
   }
 };
+
+function connectSources(container) {
+  var memorySource = container.lookup('store:main').orbitSource;
+  var jsonApiSource = container.lookup('store:jsonApi').orbitSource;
+  // Connect memorySource -> jsonApiSource (using default blocking strategy)
+  setupConnectors(memorySource, jsonApiSource);
+
+  logTransforms(memorySource, 'store:main');
+  logTransforms(jsonApiSource, 'store:jsonApi');
+}
+
+function setupConnectors(primary, secondary) {
+  new Orbit.TransformConnector(primary, secondary);
+  new Orbit.TransformConnector(secondary, primary);
+  primary.on('assistFind', secondary.find);
+}
+
+function logTransforms(source, name) {
+  source.on('didTransform', function(operation) {
+    console.log('[ORBIT.JS] [' + name + ']', operation);
+  });
+}
