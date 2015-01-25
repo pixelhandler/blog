a7645a26e66233941a6585080d66198d9cba7c2e
Bill Heaton <pixelhandler@gmail.com>
2015-01-25 01:00:00 -0800

    Update to ember-cli 0.1.9

--- client/tests/test-helper.js
+++ client/tests/test-helper.js
@@ -10,7 +10,7 @@ setResolver(resolver);
 
 document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
 
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
+QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
 var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
 document.getElementById('ember-testing-container').style.visibility = containerVisibility;
 window.testData = testData;
